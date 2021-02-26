11b196e9117831443eaf6a2b78325a74c173e386
Akbar Khusanbaev <dik@MacBook-Air-Akbar.local>
2021-02-26 16:50:51 +0500

    first-commit

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 const express = require('express')
-const axios = require('axios')
-const cors = require('cors')
+
 const app = express()
-const port = process.env.PORT
-
-app.use(cors())
-app.use(express.json())
-
-axios.post(`https://api.telegram.org/bot1421190534:AAEfEhWJoQBJqJJuPjTimcEKhQId7VtHap4/setWebhook`, {
-    url: `https://safe-savannah-20412.herokuapp.com/bot1421190534:AAEfEhWJoQBJqJJuPjTimcEKhQId7VtHap4`
-}).then(res => {
-    console.log(res.data)
-})
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: `CORS connected`})
+    res.send(`Hello YOU`)
 })
 
-app.post('/bot1421190534:AAEfEhWJoQBJqJJuPjTimcEKhQId7VtHap4', (req, res) => {
-    if (req.body.message.text === '/start') {
-        return axios.post(`https://api.telegram.org/bot1421190534:AAEfEhWJoQBJqJJuPjTimcEKhQId7VtHap4/sendMessage`, {
-            chat_id: req.body.message.chat.id,
-            text: 'Answer'
-        })
-    }
-})
+const PORT = process.env.PORT || 88
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
